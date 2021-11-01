dc72b8fc9593ac2c5e1b4973186d6014716e39ee
sara <sara.tankard@gmail.com>
2021-11-01 15:42:42 -0400

    Testing production deploy

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,13 +3,12 @@ import Link from "next/link";
 
 import Navbar from "react-bootstrap/Navbar";
 import Image from "next/image";
-import Nav from "react-bootstrap/Nav";
 
 import styles from "./Header.module.scss";
 
 const Header: NextPage = () => {
   return (
-    <header>
+    <header className={styles.header}>
       Header
     </header>
   );
