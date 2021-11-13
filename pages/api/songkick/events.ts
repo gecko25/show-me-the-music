3d798d252dbe03f1d8c509a5ead49d87615e20c8
sara <sara.tankard@gmail.com>
2021-11-13 13:13:36 -0500

    i007: This is ridiculous

--- pages/api/songkick/events.ts
+++ pages/api/songkick/events.ts
@@ -1,6 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { stringify } from "query-string";
-import { get } from "axios";
 
 import { songkick } from "@utils/queries";
 
@@ -19,42 +17,3 @@ export default async function handler(
 
   res.status(200).json(songkickResponse.data);
 }
-
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { stringify } from "query-string";
-// import { get } from "axios";
-
-// import { songkick } from "@utils/queries";
-
-// // Types
-// import { EventsResults } from "../../../types";
-
-// type Data = {
-//   name: string;
-// };
-
-// export default function handler(req, res) {
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) => {
-//   const songkickResponse = await songkick().get<EventsResults>("/events.json", {
-//     params: {
-//       ...req.query
-//     },
-//   });
-
-//   console.log(
-//     `GET: ${req.url}?${stringify(
-//       req.query
-//     )}`
-//   );
-
-//   console.log(
-//     `GET: ${songkickResponse.config.baseURL}${songkickResponse.config.url}?${stringify(
-//       songkickResponse.config.params
-//     )}`
-//   );
-
-//   res.status(200).json(songkickResponse);
-// };
