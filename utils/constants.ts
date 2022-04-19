c3298fcceb5290f4af30511b7f5919e0ea79d644
sara <sara.tankard@gmail.com>
2022-04-19 14:42:10 -0400

    i53: Adding better error handling

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,3 +2,6 @@ export const REACT_QUERY_CONFIG = {
   cacheTime: 3600000, // 1 hour
   staleTime: 3600000, // 1 hour
 };
+
+export const DEFAULT_ERROR =
+  "Shoot 😞 <br/>Something unexpected happened and we cannot load events at this time.<br/>Please refresh or try again later.";
